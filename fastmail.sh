bb1ffa4e69d10b0b99dfca1b645204a705900ae2
Callum Skeet <cskeet@fastmailteam.com>
2025-11-19 16:03:58 +1100

    Allow passing flags from config file

--- fastmail.sh
+++ fastmail.sh
@@ -4,6 +4,22 @@ export TMPDIR="$XDG_RUNTIME_DIR/app/${FLATPAK_ID:-com.fastmail.Fastmail}"
 
 declare -a FLAGS=()
 
+if [[ -f "$(dirname "${BASH_SOURCE[0]}")/fastmail.conf" ]]; then
+    CONF_FILE="$(dirname "${BASH_SOURCE[0]}")/fastmail.conf"
+elif [[ -n "$XDG_CONFIG_HOME" && -f "$XDG_CONFIG_HOME/fastmail.conf" ]]; then
+    CONF_FILE="$XDG_CONFIG_HOME/fastmail.conf"
+elif [[ -f "$HOME/.config/fastmail.conf" ]]; then
+    CONF_FILE="$HOME/.config/fastmail.conf"
+fi
+
+if [[ -n "$CONF_FILE" ]]; then
+    echo "Reading config file: $CONF_FILE"
+    # Read flags from config file, allow for comments
+    while read -r -a line_flags; do
+        FLAGS+=("${line_flags[@]}")
+    done < <(grep -v '^\s*#' "$CONF_FILE")
+fi
+
 if [[ $XDG_SESSION_TYPE == "wayland" && -z "$DISPLAY" ]]; then
     if [[ -c /dev/nvidia0 ]]; then
         echo "Using NVIDIA on Wayland, disabling gpu sandbox"
